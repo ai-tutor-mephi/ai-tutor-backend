69e61e576f02cc85ff92e14d33c2ebe4603d9280
VLmb <vlamb0809@gmail.com>
2025-09-24 13:18:50 +0300

    feat: add GlobalExceptionHandler

--- src/main/java/com/VLmb/ai_tutor_backend/exception/TokenRefreshException.java
+++ src/main/java/com/VLmb/ai_tutor_backend/exception/TokenRefreshException.java
@@ -1,7 +1,17 @@
 package com.VLmb.ai_tutor_backend.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class TokenRefreshException extends RuntimeException {
-  public TokenRefreshException(String message) {
-    super(message);
-  }
+
+    public TokenRefreshException(String message) {
+        super(message);
+    }
+
+    public TokenRefreshException(String token, String message) {
+        super(String.format("Failed for token [%s]: %s", token, message));
+    }
+
 }
