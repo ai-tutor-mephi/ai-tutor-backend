d5a1a8b937777d372003b5f97fe85f87cb0717b9
VLmb <vlamb0809@gmail.com>
2026-05-03 00:42:28 +0300

    feat: add Spring Actuator and getting health status by /actuator/health url

--- src/main/java/com/VLmb/ai_tutor_backend/shared/config/SecurityConfig.java
+++ src/main/java/com/VLmb/ai_tutor_backend/shared/config/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
                         "/v3/api-docs/**",
                         "/swagger-ui/**",
                         "/swagger-ui.html",
+                        "/actuator/health/**",
                         "/error"
                 ).permitAll()
                 .requestMatchers(POST, "/api/auth/logout").authenticated()
